7fd46b84a2c944a3b715142503aaa6534fe336ab
zaq46 <shohei.yoshikawa@sjsu.edu>
2017-11-27 02:22:12 -0800

    add mvc config and signup html

---
+++ src/main/java/project/MvcConfig.java
@@ -0,0 +1,18 @@
+package project;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/signup").setViewName("signup");
+        registry.addViewController("/").setViewName("index");
+        
+    }
+
+}
